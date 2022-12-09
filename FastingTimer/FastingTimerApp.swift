35c8715487d161983c0e4f0fcd300cdbef3f9e03
nasir <nasir.dev74@gmail.com>
2022-12-09 20:14:15 +0600

    Initial Commit

---
+++ FastingTimer/FastingTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FastingTimerApp.swift
+//  FastingTimer
+//
+//  Created by Softcent on 9/12/22.
+//
+
+import SwiftUI
+
+@main
+struct FastingTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
